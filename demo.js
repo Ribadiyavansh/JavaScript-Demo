36ad650fcac5631c3c1c8957ef9bee9b075f85f6
Ribadiyavansh <vanshribadiya3@gmail.com>
2025-05-20 16:15:56 +0530

    some importent topic des. and name

---
+++ demo.js
@@ -0,0 +1,30 @@
+//word--[harsh, good  (Thos who have no keyword)] and keyword--[ if, else, for, while ]
+//var--[ value will change ] let const--[ value will be fixed and not change ]
+//hoisting--[we use variable in the previous line of defind it]--[difind and not-defined]
+//types in js--[ primitive--{ number, string, null, undefind, boolean } and reference<(), [], {}> ]
+// conditional--[if, else, else-if]
+// LOOPS--[for and While]
+
+    // for(var i=25; i<51; i++){
+    //     console.log(i);
+    // }
+
+// Functions
+    function hello() {
+        console.log("Hello");
+    }
+    hello()
+    // TYPE:- with argument return; with argument no return; no argument no return; default argument    
+
+    // ARRAY :-= Store more than on value in the one variable 
+        // var arr = [1,2,3,4,5]; 
+        // arr[0]
+
+
+        // PUSH--[add the last value in the arr ] -- [arr.push(10)] 
+        // POP--[remove the last value from the arr] -- [arr.pop()]
+        // SHIFT--[remove the first value from the arr] -- [arr.shift()]
+        //UNSHIFT [ add a value to the starting of the arr] -- [arr.unshift(0)]
+        // SPLICE-[remove any value from any position in the arr ] -- [arr.splice(2,1)]
+
+        // PROPERTY And METHOD 
